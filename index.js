a57c829dfb3fe2f348e02d4d9a5bc6f4eaeb03e3
whysw <ysw1109@postech.ac.kr>
2020-10-22 15:45:52 +0900

    added 추가 emoji.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
+const fs = require('fs');
 const db = require("./db.json");
-
 const keys = Object.keys(db);
 
 client.on('ready', () => {
@@ -10,13 +10,29 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+    if(msg.author.bot) return;
+    if (msg.content.startsWith("추가 ")){
+        let data = msg.content.split(" ");
+        try {
+            if (data.length > 3 || !data[1] || !data[2]) throw 'invalid format';
+            fs.appendFile('db_cand.json', '    "'+data[1]+'":"'+data[2]+'",\n', err => {
+                console.log("요청 "+msg.content);
+                msg.reply("\n요청됨 "+msg.content);
+                if(err) console.error(err);
+            });
+        } catch (err) {
+            console.error(err);
+            msg.reply("\n추가 + 한글 + 이모지");
+        }
+    }
+
     for (let i=0, len = keys.length; i<len; i++) {
         let index = msg.content.indexOf(keys[i]);
         if (index == -1) continue;
 
         let timer = setTimeout(() => {
             msg.react(db[keys[i]]);
-        }, index*500);
+        }, index*200);
     }
 });
 
